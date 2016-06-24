725cf3738b0ab98ceee26c99516e6ff664692181
NgocSon2412 <sonnguyenk57@gmail.com>
2016-06-24 16:40:29 +0700

    home page

---
+++ application/language/fels/home_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['title_default'] = 'Project 1';
+$lang['name_project'] = 'Project 1';
+$lang['home_page_title'] = 'Home Page';
+$lang['home'] = 'Home';
+$lang['about'] = 'About';
+$lang['contact'] = 'Contact';
+$lang['sign_in'] = 'Sign in';
+$lang['sign_up'] = 'Sign up';
+$lang['content_1'] = 'Jumbotron heading';
+$lang['content_2'] = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.';
+$lang['footer'] = '&copy; 2016 Company, Inc.';
