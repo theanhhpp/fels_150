15a0cef3060d7623561b6400da57d2a4a0a51333
theanhhpp <theanhhpp@gmail.com>
2016-06-30 13:23:36 +0700

    categories

---
+++ application/language/fels/category_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['category_create_successful'] = 'Thêm category thành công!';
+$lang['category_create_error'] = 'Thêm category không thành công!';
+$lang['category_delete_successful'] = 'Xóa category thành công';
+$lang['category_create_error'] = 'Xóa category không thành công';
+$lang['title_create_category'] =  'Add Category';
+$lang['category_name'] = 'Name';
+$lang['category_id'] = 'ID';
+$lang['no_data_message'] =  'Không có dữ liệu';
+$lang['category_edit'] = 'Edit';
+$lang['category_delete'] = 'Delete';
+$lang['category_action'] = 'Action';
+$lang['title_edit_category'] = 'Edit';
+$lang['category_edit_successful'] = 'Sửa category thành công';
+$lang['category_edit_error']  = 'Sửa category thất bại';
+$lang['category_delete_successful'] = 'Xóa category thành công';
+$lang['category_delete_error'] = 'Xóa category thất bại';
